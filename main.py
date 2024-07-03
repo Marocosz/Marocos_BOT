72c1d174603dd09a26e473b1d229aeb03b59dba6
Marocos <marcosrodriguesepro@gmail.com>
2024-07-02 21:15:43 -0300

    Commit Inicial

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+permissoes = discord.Intents.default()
+bot = commands.Bot(command_prefix=".", intents=permissoes)
+
+
+
+
+
+
+
+bot.run("MTI1Nzc4NjE3MzI1MDg2MzE3NA.GeexZv.n4HohzyNBGssroLdZ7w3y6OnCZPKbclUxVhiXo")
